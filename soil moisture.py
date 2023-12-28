099cb5e5f7347269072d4258e6ea88c45614e082
SanduniWar <sanduni.tperera@outlook.com>
2023-12-29 00:44:32 +0100

    soil final code script

--- soil moisture.py
+++ soil moisture.py
@@ -1,23 +1,9 @@
-# SPDX-FileCopyrightText: 2021 ladyada for Adafruit Industries
-# SPDX-License-Identifier: MIT
-
+import serial
 import time
 
-import board
-
-from adafruit_seesaw.seesaw import Seesaw
-
-i2c_bus = board.I2C()  # uses board.SCL and board.SDA
-i2c_bus = board.STEMMA_I2C()  # For using the built-in STEMMA QT connector on a microcontroller
-
-ss = Seesaw(i2c_bus, addr=0x36)
+ser = serial.Serial('/dev/ttyACM0', 9600)  # Adjust the port and baud rate
 
 while True:
-    # read moisture level through capacitive touch pad
-    touch = ss.moisture_read()
-
-    # read temperature from the temperature sensor
-    temp = ss.get_temp()
-
-    print("temp: " + str(temp) + "  moisture: " + str(touch))
+    data = ser.readline().decode('utf-8').strip()
+    print(f'Received from Arduino: {data}')
     time.sleep(1)
