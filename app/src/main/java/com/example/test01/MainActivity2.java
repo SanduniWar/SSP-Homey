9f90b72f737384a3ae8e868101642031e037504f
Prabhashim <154088534+Prabhashim@users.noreply.github.com>
2024-01-02 06:56:49 -0800

    First Change

--- app/src/main/java/com/example/test01/MainActivity2.java
+++ app/src/main/java/com/example/test01/MainActivity2.java
@@ -9,19 +9,9 @@ import android.widget.Button;
 import android.widget.Switch;
 import android.widget.TextView;
 
-import org.eclipse.paho.client.mqttv3.IMqttDeliveryToken;
-import org.eclipse.paho.client.mqttv3.MqttCallback;
-import org.eclipse.paho.client.mqttv3.MqttClient;
-import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
-import org.eclipse.paho.client.mqttv3.MqttMessage;
-import org.eclipse.paho.client.mqttv3.persist.MemoryPersistence;
-
 public class MainActivity2 extends AppCompatActivity {
     Button bkh1button;
     TextView txv_temp;
-    private static final String BROKER = "tcp://test.mosquitto.org:1883";  // change
-    private static final String TOPIC = "ssp_homey/checkTemp";  //  // change
-    private static final String CLIENT_ID = MqttClient.generateClientId(); //  // change
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,7 +20,6 @@ public class MainActivity2 extends AppCompatActivity {
 
         bkh1button = findViewById(R.id.bkh1button);
         txv_temp = findViewById(R.id.indoortemp);
-        connectToMQTTBroker();
 
         bkh1button.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -39,50 +28,5 @@ public class MainActivity2 extends AppCompatActivity {
                 startActivity(intent);
             }
         });
-    }
-
-    private void connectToMQTTBroker() { // change
-        try {
-            MqttClient mqttClient = new MqttClient(BROKER, CLIENT_ID, new MemoryPersistence()); // change
-            MqttConnectOptions options = new MqttConnectOptions(); //change
-            options.setCleanSession(true);
-
-            mqttClient.connect(options);
-
-            // Set the callback for message handling
-            mqttClient.setCallback(new MqttCallback() {
-                @Override
-                public void connectionLost(Throwable cause) {
-                    // Handle connection lost
-                }
-
-                @Override
-                public void messageArrived(String topic, MqttMessage message) throws Exception { // change
-                    // Update the TextView with the received message
-                    String payload = new String(message.getPayload());  //cnahe
-                    System.out.println("Incoming message: " + payload); // cnahge
-                    runOnUiThread(new Runnable() {
-                        @Override
-                        public void run() {
-                            updateTextView(payload);  //change
-                        }
-                    });
-                }
-                @Override
-                public void deliveryComplete(IMqttDeliveryToken token) {
-                    // Delivery complete
-                }
-            });
-
-            // Subscribe to the topic
-            mqttClient.subscribe(TOPIC, 0);
-
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-
-    private void updateTextView(String payload) {  //cnahge
-        txv_temp.setText(payload);
     }
 }
