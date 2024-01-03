a72405371e7fc49134752023c2f9e781f2f01cfc
Prabhashim <154088534+Prabhashim@users.noreply.github.com>
2024-01-02 18:17:53 -0800

    Connection Updated

--- app/src/main/java/com/example/test01/MainActivity2.java
+++ app/src/main/java/com/example/test01/MainActivity2.java
@@ -6,12 +6,31 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CompoundButton;
 import android.widget.Switch;
 import android.widget.TextView;
 
+import org.eclipse.paho.client.mqttv3.IMqttClient;
+import org.eclipse.paho.client.mqttv3.IMqttDeliveryToken;
+import org.eclipse.paho.client.mqttv3.MqttCallback;
+import org.eclipse.paho.client.mqttv3.MqttClient;
+import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
+import org.eclipse.paho.client.mqttv3.MqttMessage;
+import org.eclipse.paho.client.mqttv3.persist.MemoryPersistence;
+
+
+
 public class MainActivity2 extends AppCompatActivity {
     Button bkh1button;
-    TextView txv_temp;
+    TextView txvTemp;
+    String heaterStatus;
+    String lightStatus;
+
+    private static final String broker = "tcp://test.mosquitto.org:1883";
+    private static final String topicForTemp = "ssp_homey/checkTemp";  // Topic for temperature
+    private static final String topicForHeater = "ssp_homey/heaterTurnOnOff";  // Topic for heater
+    private static final String topicForLight = "ssp_homey/lightTurnOnOff";  // Topic for light
+    private static final String clientId = MqttClient.generateClientId();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,7 +38,44 @@ public class MainActivity2 extends AppCompatActivity {
         setContentView(R.layout.activity_main2);
 
         bkh1button = findViewById(R.id.bkh1button);
-        txv_temp = findViewById(R.id.indoortemp);
+        txvTemp = findViewById(R.id.txtIndoorTemp);
+        Switch heaterToggleButton = findViewById(R.id.btnToggle2);
+        Switch lightToggleButton = findViewById(R.id.btnToggle1);
+
+        connectToMQTT("off");
+        heaterToggleButton.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+
+                if (isChecked) {
+
+                    heaterStatus = "on";
+                    heaterTurnonOff(heaterStatus);
+                } else {
+
+                    heaterStatus = "off";
+                    heaterTurnonOff(heaterStatus);
+                }
+
+            }
+        });
+
+        lightToggleButton.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+
+                if (isChecked) {
+
+                    lightStatus = "on";
+                    lightTurnonOff(lightStatus);
+                } else {
+
+                    lightStatus = "off";
+                    lightTurnonOff(lightStatus);
+                }
+
+            }
+        });
 
         bkh1button.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -29,4 +85,93 @@ public class MainActivity2 extends AppCompatActivity {
             }
         });
     }
+
+
+    private void connectToMQTT(String HStatus) {
+        try {
+
+            MqttClient mqttClient = new MqttClient(broker, clientId, new MemoryPersistence());
+            MqttConnectOptions connectOptions = new MqttConnectOptions();
+            connectOptions.setCleanSession(true);
+
+            mqttClient.connect(connectOptions);
+
+
+            mqttClient.setCallback(new MqttCallback() {
+                @Override
+                public void connectionLost(Throwable cause) {
+                }
+
+                @Override
+                public void messageArrived(String topic, MqttMessage message) throws Exception {
+                    String tempValue = new String(message.getPayload());
+                    System.out.println("Temp Value from Sensor: " + tempValue);
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            updateTextView(tempValue);
+                        }
+                    });
+
+                    float temperatureValue;
+                    String heaterStatus ;
+
+                    temperatureValue = Float.parseFloat(tempValue);
+
+
+                   if (temperatureValue < 1.0) {
+
+                        heaterStatus = "on";
+                       heaterTurnonOff(heaterStatus);
+                    }
+
+                }
+
+                @Override
+                public void deliveryComplete(IMqttDeliveryToken token)
+                {
+                }
+            });
+
+            mqttClient.subscribe(topicForTemp, 0);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void updateTextView(String temp) {
+
+        txvTemp.setText(temp);
+    }
+
+    private void heaterTurnonOff(String hstatus){
+        try {
+            MqttClient mqttClient = new MqttClient(broker, clientId, new MemoryPersistence());
+            MqttConnectOptions connectOptions = new MqttConnectOptions();
+            connectOptions.setCleanSession(true);
+
+            mqttClient.connect(connectOptions);
+            mqttClient.publish(topicForHeater, hstatus.getBytes(),0, false);
+        }catch(Exception ex){
+            ex.printStackTrace();
+        }
+
+    }
+
+    private void lightTurnonOff(String lstatus){
+        try {
+            MqttClient mqttClient = new MqttClient(broker, clientId, new MemoryPersistence());
+            MqttConnectOptions connectOptions = new MqttConnectOptions();
+            connectOptions.setCleanSession(true);
+
+            mqttClient.connect(connectOptions);
+            mqttClient.publish(topicForLight, lstatus.getBytes(),0, false);
+        }catch(Exception ex){
+            ex.printStackTrace();
+        }
+
+    }
+
+
 }
